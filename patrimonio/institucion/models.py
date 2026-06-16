a35872c5bd0ce8c24c03544ce9c3a1b292fd5e53
Paulalopez12 <paulalopezacaro1201@gmail.com>
2026-06-16 11:59:29 -0500

    2

--- patrimonio/institucion/models.py
+++ patrimonio/institucion/models.py
@@ -13,7 +13,15 @@ class Museo(models.Model):
 class GuiaMuseo(models.Model):
     nombre_completo = models.CharField(max_length=100)
     anios_experiencia_guia = models.IntegerField()
-    idiomas_hablados = models.CharField(max_length=200)
-    museo = models.Fore
-    
+    idiomas_hablados = models.CharField(max_length=100)
+    museo = models.ForeignKey(Museo, on_delete=models.CASCADE, related_name='guias')
 
+    def __str__(self):
+        return self.nombre_completo
+
+class Exhibicion(models.Model):
+    titulo_exhibicion = models.CharField(max_length=100)
+    duracion_meses = models.IntegerField()
+    costo_produccion = models.DecimalField(max_digits=10, decimal_places=2)
+    tematica = models.CharField(max_length=100)
+    guia = models.ForeignKey(GuiaMuseo, on_delete=models.CASCADE, related_name='exhibiciones')
